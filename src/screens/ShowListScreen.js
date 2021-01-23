bfd042ff4c6de3d99d8fe9247b7ee1ef43e4bec6
Andoni Fernandez Pina <andonifp@ext.inditex.com>
2021-01-24 00:19:18 +0100

    Saio lista

--- src/screens/ShowListScreen.js
+++ src/screens/ShowListScreen.js
@@ -6,35 +6,49 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {Image, SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, SafeAreaView, TextInput, StyleSheet, View, StatusBar} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import ShowList from '../components/ShowList';
 import useFetchPrograms from '../hooks/useFetchPrograms';
+import {useNavigation} from '@react-navigation/native';
 
 const Main: () => React$Node = () => {
     const list = useFetchPrograms();
+    const navigation = useNavigation();
+    const [text, setText] = useState('');
 
     if (list === 'LOADING') {
         return (
             <SafeAreaView>
+                <StatusBar hidden />
                 <View style={styles.container}>
-                    <Image source={require('../images/splash.png')} style={styles.backgroundImage}/>
+                    <Image
+                        source={require('../images/splash.png')}
+                        style={styles.backgroundImage}
+                    />
                 </View>
             </SafeAreaView>
         );
     }
 
+    const filteredList = list.filter(item => {
+        return item.title.includes(text);
+    });
     return (
-        <SafeAreaView>
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={styles.scrollView}>
-                <View style={styles.body}>
-                    <ShowList list={list}/>
-                </View>
-            </ScrollView>
+        <SafeAreaView style={{flex: 1}}>
+            <StatusBar hidden />
+            <TextInput
+                style={{height: 80, fontSize: 30, paddingLeft: 20}}
+                placeholder="Saioa bilatu..."
+                onChangeText={text => setText(text)}
+                defaultValue={text}
+            />
+            <ShowList
+                list={filteredList}
+                navigation={navigation}
+            />
         </SafeAreaView>
     );
 };
