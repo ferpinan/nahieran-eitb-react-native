b4920980bf91911320fb76354409047072b4a1c9
Andoni Fernandez Pina <andonifp@ext.inditex.com>
2021-01-24 00:56:08 +0100

    Episodes as grid

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -14,8 +14,10 @@ import {
     StyleSheet,
     Text,
     View,
-    Image
+    Image,
+    Dimensions
 } from 'react-native';
+import {FlatGrid} from 'react-native-super-grid';
 
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 import useFetchProgramDetail from '../hooks/useFetchProgramDetail';
@@ -25,99 +27,53 @@ const ShowScreen: () => React$Node = ({navigation, route}) => {
     let useFetchProgramDetail1 = useFetchProgramDetail(routeElement);
     if (useFetchProgramDetail1 === 'LOADING') {
         return (
-            <View style={styles.body}>
-                <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>LOADING</Text>
+            <View>
+                <View>
+                    <Text>LOADING</Text>
                 </View>
             </View>
         );
     }
 
+    const win = Dimensions.get('window');
     return (
         <>
             <SafeAreaView>
-                <ScrollView
-                    contentInsetAdjustmentBehavior="automatic"
-                    style={styles.scrollView}>
-                    <View style={styles.body}>
-                        <Text style={styles.sectionTitle}>
-                            {route.params.title}
-                        </Text>
-                        <View style={styles.sectionContainer}>
-                            {useFetchProgramDetail1.member.map(
-                                (member, index) => (
-                                    <>
-                                        <Text
-                                            key={`list_${index}`}
-                                            onPress={() =>
-                                                navigation.navigate(
-                                                    'EpisodeDetails',
-                                                    member,
-                                                )
-                                            }>
-                                            {member.title}
-                                        </Text>
-                                    </>
-                                ),
-                            )}
+                <FlatGrid
+                    itemDimension={130}
+                    data={useFetchProgramDetail1.member.reverse()}
+                    renderItem={({item}) => (
+                        <View style={{align: 'center'}}>
+                            <Image
+                                style={{
+                                    // flex: 1,
+                                    alignSelf: 'stretch',
+                                    width: win.width / 2 - 20,
+                                    height: 150,
+                                }}
+                                // resizeMode={'contain'}
+                                source={{
+                                    uri: item.episode_image,
+                                }}
+                            />
+                            <Text
+                                style={{
+                                    alignSelf: 'center'
+                                }}
+                                onPress={() =>
+                                    navigation.navigate(
+                                        'EpisodeDetails',
+                                        item,
+                                    )
+                                }>
+                                {item.title}
+                            </Text>
                         </View>
-                    </View>
-                </ScrollView>
+                    )}
+                />
             </SafeAreaView>
         </>
     );
 };
-
-const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    engine: {
-        position: 'absolute',
-        right: 0,
-    },
-    body: {
-        backgroundColor: Colors.white,
-        flex: 1,
-    },
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-        color: Colors.dark,
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-    footer: {
-        color: Colors.dark,
-        fontSize: 12,
-        fontWeight: '600',
-        padding: 4,
-        paddingRight: 12,
-        textAlign: 'right',
-    },
-    container: {
-        paddingTop: 50,
-    },
-    tinyLogo: {
-        flex: 1,
-        width: '50%',
-        height: '50%',
-    },
-    logo: {
-        width: 66,
-        height: 58,
-    }
-});
 
 export default ShowScreen;
